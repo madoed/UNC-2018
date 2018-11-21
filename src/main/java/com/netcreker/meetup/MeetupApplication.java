de3a9f4c64d7cc44b9eceb8c74199b191c24a5ea
Andrey <yudin.andrey.d@gmail.com>
2018-11-21 09:00:14 +0300

    springboot init commit

---
+++ src/main/java/com/netcreker/meetup/MeetupApplication.java
@@ -0,0 +1,12 @@
+package com.netcreker.meetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetupApplication.class, args);
+    }
+}
