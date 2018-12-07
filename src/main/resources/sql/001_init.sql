2647937d3d1d75b82cb0edc7efc05aa6214664e4
Andrey <yudin.andrey.d@gmail.com>
2018-12-07 10:14:17 +0300

    change init.sql for postgresql

--- src/main/resources/sql/001_init.sql
+++ src/main/resources/sql/001_init.sql
@@ -1,120 +1,96 @@
-CREATE TABLE attributes (
-                          attr_id     NUMBER(6) NOT NULL,
-                          attr_name   VARCHAR2(20) NOT NULL,
-                          attr_type   NUMBER(1) NOT NULL
+DROP SCHEMA IF EXISTS meetupdb CASCADE ;
+create schema meetupdb;
+
+set search_path to meetupdb;
+
+create sequence global_id_sequence;
+
+create or replace function id_generator(out result bigint) as $$
+  declare
+  our_epoch bigint := 10000;
+seq_id bigint;
+now_millis bigint;
+-- the id of this db shard, must be set for each
+-- schema shard you have - you could pass this as a parameter too
+shard_id int := 1;
+begin
+  select nextval('global_id_sequence') % 1024 into seq_id;
+
+  select floor(extract(epoch from clock_timestamp()) * 1000) into now_millis;
+  result := (now_millis - our_epoch) << 23;
+  result := result | (shard_id << 10);
+  result := result | (seq_id);
+end;
+$$ language plpgsql;
+
+create table attributes
+(
+  attr_id   bigint primary key default id_generator(),
+  attr_name varchar(20) unique not null,
+  attr_type int  not null
 );
 
-ALTER TABLE attributes ADD CONSTRAINT attributes_pk PRIMARY KEY ( attr_id );
-
-ALTER TABLE attributes ADD CONSTRAINT attr_name UNIQUE ( attr_name );
-
-CREATE TABLE obj_attributes (
-                              object_type_id   NUMBER(6) NOT NULL,
-                              attr_id          NUMBER(6) NOT NULL
+create table obj_attributes
+(
+  object_type_id bigint not null,
+  attr_id        bigint not null
 );
 
-CREATE TABLE obj_types (
-                         object_type_id   NUMBER(6) NOT NULL,
-                         name             VARCHAR2(20) NOT NULL
+create table obj_types
+(
+  object_type_id bigint primary key default id_generator(),
+  name           varchar(20) unique not null
 );
 
-ALTER TABLE obj_types ADD CONSTRAINT obj_types_pk PRIMARY KEY ( object_type_id );
-
-ALTER TABLE obj_types ADD CONSTRAINT obj_types_ak_1 UNIQUE ( name );
-
-CREATE TABLE objects (
-                       object_id        NUMBER(6) NOT NULL,
-                       object_type_id   NUMBER(6) NOT NULL,
-                       object_name      VARCHAR2(70)
+create table objects
+(
+  object_id      bigint primary key default id_generator(),
+  object_type_id bigint not null,
+  object_name    varchar(70)
 );
 
-ALTER TABLE objects ADD CONSTRAINT objects_pk PRIMARY KEY ( object_id );
-
-CREATE TABLE params (
-                      object_id   NUMBER(6) NOT NULL,
-                      attr_id     NUMBER(6) NOT NULL,
-                      value       VARCHAR2(4000 CHAR),
-                      data        BLOB
+create table params
+(
+  object_id bigint not null,
+  attr_id  bigint not null,
+  value     varchar(4000)
 );
 
-CREATE TABLE references (
-                          object_id   NUMBER(6) NOT NULL,
-                          attr_id     NUMBER(6) NOT NULL,
-                          reference   NUMBER(6) NOT NULL
+create table refer
+(
+  object_id bigint not null,
+  attr_id   bigint not null,
+  reference bigint not null
 );
 
-ALTER TABLE obj_attributes
-  ADD CONSTRAINT obj_attributes_attributes FOREIGN KEY ( attr_id )
-    REFERENCES attributes ( attr_id );
+alter table obj_attributes
+  add constraint obj_attributes_attributes foreign key (attr_id)
+    references attributes (attr_id);
 
-ALTER TABLE obj_attributes
-  ADD CONSTRAINT obj_attributes_obj_types FOREIGN KEY ( object_type_id )
-    REFERENCES obj_types ( object_type_id );
+alter table obj_attributes
+  add constraint obj_attributes_obj_types foreign key (object_type_id)
+    references obj_types (object_type_id);
 
-ALTER TABLE objects
-  ADD CONSTRAINT obj_types_objects FOREIGN KEY ( object_type_id )
-    REFERENCES obj_types ( object_type_id );
+alter table objects
+  add constraint obj_types_objects foreign key (object_type_id)
+    references obj_types (object_type_id);
 
-ALTER TABLE references
-  ADD CONSTRAINT objects_references FOREIGN KEY ( reference )
-    REFERENCES objects ( object_id );
+alter table refer
+  add constraint objects_references foreign key (reference)
+    references objects (object_id);
 
-ALTER TABLE params
-  ADD CONSTRAINT params_attributes FOREIGN KEY ( attr_id )
-    REFERENCES attributes ( attr_id );
+alter table params
+  add constraint params_attributes foreign key (attr_id)
+    references attributes (attr_id);
 
-ALTER TABLE params
-  ADD CONSTRAINT params_objects FOREIGN KEY ( object_id )
-    REFERENCES objects ( object_id );
+alter table params
+  add constraint params_objects foreign key (object_id)
+    references objects (object_id);
 
-ALTER TABLE references
-  ADD CONSTRAINT references_attributes FOREIGN KEY ( attr_id )
-    REFERENCES attributes ( attr_id );
+alter table refer
+  add constraint references_attributes foreign key (attr_id)
+    references attributes (attr_id);
 
-ALTER TABLE references
-  ADD CONSTRAINT references_objects FOREIGN KEY ( object_id )
-    REFERENCES objects ( object_id );
-
-
-
--- Oracle SQL Developer Data Modeler Summary Report:
---
--- CREATE TABLE                             6
--- CREATE INDEX                             0
--- ALTER TABLE                             13
--- CREATE VIEW                              0
--- ALTER VIEW                               0
--- CREATE PACKAGE                           0
--- CREATE PACKAGE BODY                      0
--- CREATE PROCEDURE                         0
--- CREATE FUNCTION                          0
--- CREATE TRIGGER                           0
--- ALTER TRIGGER                            0
--- CREATE COLLECTION TYPE                   0
--- CREATE STRUCTURED TYPE                   0
--- CREATE STRUCTURED TYPE BODY              0
--- CREATE CLUSTER                           0
--- CREATE CONTEXT                           0
--- CREATE DATABASE                          0
--- CREATE DIMENSION                         0
--- CREATE DIRECTORY                         0
--- CREATE DISK GROUP                        0
--- CREATE ROLE                              0
--- CREATE ROLLBACK SEGMENT                  0
--- CREATE SEQUENCE                          0
--- CREATE MATERIALIZED VIEW                 0
--- CREATE SYNONYM                           0
--- CREATE TABLESPACE                        0
--- CREATE USER                              0
---
--- DROP TABLESPACE                          0
--- DROP DATABASE                            0
---
--- REDACTION POLICY                         0
---
--- ORDS DROP SCHEMA                         0
--- ORDS ENABLE SCHEMA                       0
--- ORDS ENABLE OBJECT                       0
---
--- ERRORS                                   0
--- WARNINGS                                 0
+alter table refer
+  add constraint references_objects foreign key (object_id)
+    references objects (object_id);
