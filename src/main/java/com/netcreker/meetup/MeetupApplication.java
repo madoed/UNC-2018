532ebb65197e4966343896dd0021cd8442b9d8b1
Anastasia Ilyina <ilyina.a.d@yandex.com>
2019-01-15 19:24:37 +0300

    app run

--- src/main/java/com/netcreker/meetup/MeetupApplication.java
+++ src/main/java/com/netcreker/meetup/MeetupApplication.java
@@ -5,11 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MeetupApplication {
-
-
-    
     public static void main(String[] args) {
         SpringApplication.run(MeetupApplication.class, args);
-
     }
 }
