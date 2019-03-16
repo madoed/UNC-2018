a7bc3d99f4c292004890c152515c3427368aed5a
ussur <ilyina.a.d@yandex.com>
2019-03-16 22:57:19 +0300

    fns

---
+++ src/main/java/com/netcreker/meetup/entity/fns/Document.java
@@ -0,0 +1,8 @@
+package com.netcreker.meetup.entity.fns;
+
+import lombok.Data;
+
+@Data
+public class Document {
+    private Receipt receipt;
+}
---
+++ src/main/java/com/netcreker/meetup/entity/fns/Item.java
@@ -0,0 +1,11 @@
+package com.netcreker.meetup.entity.fns;
+
+import lombok.Data;
+
+@Data
+public class Item {
+    private Integer sum;
+    private Integer quantity;
+    private String name;
+    private Integer price;
+}
